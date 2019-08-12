4fb5eb21bc382701f8afdceac2beb74d4416b264
mcuking <mcuking.tang@gmail.com>
2019-08-12 11:50:34 +0800

    feat: vant 引入方式从 cdn 改为按需引入，并支持定制 vant 样式主题

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        style: name => `${name}/style/less`
+      },
+      "vant"
+    ]
+  ],
   presets: ["@vue/app"]
 };
--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,26 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 const IS_DEV = NODE_ENV === "development";
 
 module.exports = {
-  css: { extract: false },
   outputDir: `dist/`,
   productionSourceMap: !IS_DEV,
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          green: "#000",
+          "button-primary-background-color": "#1989fa",
+          "button-primary-border-color": "#1989fa"
+        }
+      }
+    }
+  },
   configureWebpack: {
     entry: `./src/main.ts`,
     output: {
       libraryExport: "default"
     },
     externals: {
-      vue: "Vue",
-      vant: "vant"
-    },
-    plugins: {}
+      vue: "Vue"
+    }
   }
 };
