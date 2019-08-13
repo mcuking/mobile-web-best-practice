8e06b27fcf1c32fe3398fb715db120cd22c31bbb
mcuking <mcuking.tang@gmail.com>
2019-08-13 11:23:41 +0800

    fix: 修改 eslint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: ['plugin:vue/essential', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    quotes: [0, 'single']
+    quotes: ['error', 'single']
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
