c8639b6279425ad55951935de57a519b2bfeba5a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-13 07:10:05 +0000

    Deploy mcuking/mobile-web-best-practice to github.com/mcuking/mobile-web-best-practice.git:gh-pages

--- src/gobal.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $bus: any;
-  }
-}
--- src/utils/global-method.ts
+++
@@ -1,9 +0,0 @@
-import bus from './bus';
-
-export default {
-  install(Vue: any) {
-    Object.defineProperty(Vue.prototype, '$bus', {
-      get: () => bus,
-    });
-  },
-};
