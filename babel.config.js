9d08beca84761688c24a3655b8ab925c91d0950d
mcuking <mcuking.tang@gmail.com>
2019-08-13 11:26:22 +0800

    feat: 引入 babel-plugin-error-report   @sentry/webpack-plugin

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,33 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const IS_DEV = NODE_ENV === 'development';
+
+const plugins = [
+  [
+    'import',
+    {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: name => `${name}/style/less`
+    },
+    'vant'
+  ]
+];
+
+if (!IS_DEV) {
+  plugins.push([
+    'try-catch-error-report',
+    {
+      expression: 'window.$sentry.log',
+      needFilename: true,
+      needLineNo: true,
+      needColumnNo: false,
+      needContext: true,
+      exclude: ['node_modules']
+    }
+  ]);
+}
+
 module.exports = {
-  plugins: [
-    [
-      "import",
-      {
-        libraryName: "vant",
-        libraryDirectory: "es",
-        style: name => `${name}/style/less`
-      },
-      "vant"
-    ],
-    [
-      "try-catch-error-report",
-      {
-        expression: "window.$sentry.log",
-        needFilename: true,
-        needLineNo: true,
-        needColumnNo: false,
-        needContext: true,
-        exclude: ["node_modules"]
-      }
-    ]
-  ],
-  presets: ["@vue/app"]
+  plugins,
+  presets: ['@vue/app']
 };
--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,45 @@
-const webpack = require("webpack");
-const NODE_ENV = process.env.NODE_ENV || "development";
-const IS_DEV = NODE_ENV === "development";
+const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const IS_DEV = NODE_ENV === 'development';
+const SentryPlugin = require('@sentry/webpack-plugin');
+const VERSION = require('./package.json').version;
+
+const plugins = [];
+
+if (!IS_DEV) {
+  plugins.push(
+    new SentryPlugin({
+      release: VERSION,
+      include: './dist/static/js',
+      urlPrefix: '~/static/js/',
+      ignore: ['node_modules']
+    })
+  );
+}
 
 module.exports = {
-  outputDir: `dist/`,
+  outputDir: 'dist/',
+  assetsDir: 'static',
   productionSourceMap: !IS_DEV,
   css: {
+    extract: false,
     loaderOptions: {
       less: {
         modifyVars: {
-          green: "#000",
-          "button-primary-background-color": "#1989fa",
-          "button-primary-border-color": "#1989fa"
+          // "button-primary-background-color": "#1989fa",
+          // "button-primary-border-color": "#1989fa"
         }
       }
     }
   },
   configureWebpack: {
-    entry: `./src/main.ts`,
+    entry: './src/main.ts',
     output: {
-      libraryExport: "default"
+      libraryExport: 'default'
     },
     externals: {
-      vue: "Vue"
-    }
+      vue: 'Vue'
+    },
+    plugins
   }
 };
