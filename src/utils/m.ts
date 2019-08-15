c54c151061b222f83ee38eb5ff8ab44d6470221e
mcuking <mcuking.tang@gmail.com>
2019-08-15 11:16:41 +0800

    feat: 引入 mem 对部分接口请求进行缓存

---
+++ src/utils/m.ts
@@ -0,0 +1,13 @@
+import mem from 'mem';
+
+/**
+ * @param {MemOption} - mem 配置项
+ * @return {Function} - 装饰器
+ */
+export default function m(options: AnyObject) {
+  return (target: AnyObject, name: string, descriptor: PropertyDescriptor) => {
+    const oldValue = descriptor.value;
+    descriptor.value = mem(oldValue, options);
+    return descriptor;
+  };
+}
