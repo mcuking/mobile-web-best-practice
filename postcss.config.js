f9b920167e4f82fcd55f290fa89d18eb7b16f563
mcuking <mcuking.tang@gmail.com>
2019-08-19 16:48:52 +0800

    feat: 添加 postcss-px-to-viewport  通过 vw 方案实现样式兼容

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 3,
+      viewportUnit: 'vw',
+      // 该项在使用 Circle 组件时需要
+      // 原因参见 https://github.com/youzan/vant/issues/1948
+      selectorBlackList: ['.ignore', '.hairlines', 'van-circle__layer'],
+      minPixelValue: 1,
+      mediaQuery: false
+    }
   }
 };
